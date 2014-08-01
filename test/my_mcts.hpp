4af36848db57689c91c331fa0f134a8af478b55d
pandaant <apandaant@gmail.com>
2014-08-01 11:48:45 +0000

    forgot to ad my_mcts.hpp

---
+++ test/my_mcts.hpp
@@ -0,0 +1,14 @@
+#ifndef MY_MCTS_H
+#define MY_MCTS_H
+
+#include <mcts.h>
+#include "result.h"
+
+class RPSMCTS : public MCTS<RockPaperScissors, RPSConfig, RPSNode, RPSResult> {
+  RPSResult evaluate() {
+    for (int i = 0; i < 100; ++i)
+      iterate();
+  }
+};
+
+#endif
